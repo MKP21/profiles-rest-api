105fa23115df7010e0567d1b250ffdca43dc31be
MKP21 <meetknpatel2@gmail.com>
2020-09-21 23:53:27 -0400

    added filters

--- app/profiles_api/views.py
+++ app/profiles_api/views.py
@@ -7,6 +7,8 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
 
+from rest_framework import filters
+
 class HelloApiView(APIView):
     """Test API View"""
     serializer_class = serializers.HelloSerializer
@@ -103,3 +105,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
